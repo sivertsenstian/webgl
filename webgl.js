f2bf3012b4f186b42d26e31928b56c2fa71a1243
Stian Sivertsen <sivertsenstian@gmail.com>
2018-06-27 00:07:43 +0200

    Light!

--- webgl.js
+++ webgl.js
@@ -14,10 +14,19 @@ const init = () => {
   scene = new THREE.Scene();
 
   geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-  material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+  // material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+  material = new THREE.MeshPhongMaterial({ color: 0xff0000 });
 
   mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
+
+  //Light
+  var light = new THREE.AmbientLight(0x404040); // soft white light
+  scene.add(light);
+
+  // White directional light at half intensity shining from the top.
+  var directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+  scene.add(directionalLight);
 
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
