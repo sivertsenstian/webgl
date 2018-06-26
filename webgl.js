6d8d7c152867166a423299d0a36391b01cc3e865
Stian Sivertsen <sivertsenstian@gmail.com>
2018-06-27 01:30:11 +0200

    direction

--- webgl.js
+++ webgl.js
@@ -39,6 +39,23 @@ const createCube = (x, y, z) => {
   return mesh;
 };
 
+const directionCube = (v1, v2) => {
+  let material = new THREE.MeshBasicMaterial({ map: texture }),
+    direction = new THREE.Vector3().subVectors(v1, v2),
+    orientation = new THREE.Matrix4();
+  orientation.lookAt(v1, v2, new THREE.Object3D().up);
+  orientation.multiply(
+    new THREE.Matrix4().set(1, 0, 0, 0, 0, 0, 1, 0, 0, -1, 0, 0, 0, 0, 0, 1)
+  );
+  let geometry = new THREE.BoxGeometry(1, direction.length(), 1),
+    mesh = new THREE.Mesh(geometry, material);
+
+  mesh.applyMatrix(orientation);
+  mesh.position.set((v1.x + v2.x) / 2, (v1.y + v2.y) / 2, (v1.z + v2.z) / 2);
+  mesh.name = "data";
+  return mesh;
+};
+
 const init = () => {
   camera = new THREE.PerspectiveCamera(
     70,
@@ -63,9 +80,13 @@ const init = () => {
   scene.add(yline);
 
   //Data
-  for (let i = 0; i < path.length; i++) {
-    let [x, y, z] = path[i],
-      c = createCube(x, y, z);
+  for (let i = 0; i < path.length - 1; i++) {
+    let [x1, y1, z1] = path[i],
+      [x2, y2, z2] = path[i + 1],
+      c = directionCube(
+        new THREE.Vector3(x1, y1, z1),
+        new THREE.Vector3(x2, y2, z2)
+      );
     scene.add(c);
   }
 
